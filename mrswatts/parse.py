8bf4226f5948265ea5021f45b4a5b3f00c261df1
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-24 00:56:51 -0600

    Update parse.py

--- mrswatts/parse.py
+++ mrswatts/parse.py
@@ -1,3 +1,11 @@
+try:
+    import android
+except:
+    pass
+
+import sys
+
+
 def messages(tag):
     summary = []
     lines = tag.splitlines()
@@ -6,3 +14,26 @@ def messages(tag):
         data = line.split('|')[6].replace('@cabin', '').strip()
         summary.append(person + ': ' + data)
     return "\n".join(summary)
+
+if __name__ == "__main__":
+    try:
+        droid = android.Android()
+    except:
+        pass
+    try:
+        lines = droid.getIntent().result[u'extras'][u'%lines']
+    except:
+        droid.makeToast('data missing')
+        sys.exit(1)
+
+    try:
+        line_number = droid.getIntent().result[u'extras'][u'%line_number']
+    except:
+        line_number = 0
+
+    try:
+        position = droid.getIntent().result[u'extras'][u'%position']
+    except:
+        position = 0
+
+    print(lines.splitlines()[line_number].split('|')[position].strip())
